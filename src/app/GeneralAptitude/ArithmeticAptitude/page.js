89e46f6ca84f421c8bf933d65d3daf62ff883a24
Irzum Shahid <irzum.shahid@sprintech.digitalm>
2023-07-16 00:00:31 +0500

    enabled context api, created cards for topic selection

--- src/app/GeneralAptitude/ArithmeticAptitude/page.js
+++ src/app/GeneralAptitude/ArithmeticAptitude/page.js
@@ -1,17 +1,9 @@
 "use client";
 
 import TopicSelectionCard from "@/components/TopicSelectionCard";
-import React, { useEffect } from "react";
 import FOLDER_IMAGE from "../../../assets/folder.png";
-import { myContext } from "@/context/Store";
 
 const ArithmeticAptitude = () => {
-  const { activeNav } = myContext();
-
-  useEffect(() => {
-    console.log(activeNav);
-  }, []);
-
   return (
     <div className="flex flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row gap-5 mb-5">
       <div className="w-full lg:w-1/3 xl:w-1/3 2xl:w-1/3 flex flex-col gap-5">
